336073353543982f2b2952364ee2acdcb985c100
Aki <sample@gmail.com>
2015-03-25 16:49:48 +0900

    公也

---
+++ app/src/main/java/aki/aso/ac/jp/gitsample/Home.java
@@ -0,0 +1,9 @@
+package aki.aso.ac.jp.gitsample;
+
+/**
+ * Created by Aki on 15/03/25.
+ */
+public class Home {
+    private static final String TAG = Home.class.getSimpleName();
+    private final Home self = this;
+}
